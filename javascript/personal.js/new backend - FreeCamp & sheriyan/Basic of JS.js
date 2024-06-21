dbf6d421e378cdf00e600704ba479695c5d4f82b
jagmohan <jimmyfalco007@gmail.com>
2024-06-21 09:49:55 +0530

    done

--- javascript/personal.js/new backend - FreeCamp & sheriyan/Basic of JS.js
+++ javascript/personal.js/new backend - FreeCamp & sheriyan/Basic of JS.js
@@ -32,3 +32,30 @@ let find = a.find(val =>{
 })
 // console.log(find)
 // --------------------------------------------------------
+// Object
+// to ways to access obj.prop = obj["prop"]
+// Object.freeze(obj) - it will make the object immutable else value can be changed
+
+let obj = {
+    name: "John",
+    age: 20,
+    address: {
+        city: "Delhi",
+        state: "DL"
+    }
+}
+// Object.freeze(obj)
+// obj.age = 13
+// console.log(obj.age,obj["address"])
+// --------------------------------------------------------
+// function has length
+// No of params in fnc = fnc.length
+// bcoz function is an object
+// everthing in JS is basically an object
+
+let add = (a) => {
+    let c = "hello";
+    return c;
+}
+console.log(add())
+// --------------------------------------------------------

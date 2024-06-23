22cd9aeb4174916348667749cd1e17dedd8e452c
jagmohan <jimmyfalco007@gmail.com>
2024-06-23 16:06:45 +0530

    node init

---
+++ javascript/personal.js/new backend - FreeCamp & sheriyan/node.js
@@ -0,0 +1,10 @@
+// node.js is a JS Runtime Environment
+// JS does not have capability to create backend
+// Chrome V8 engine is made in C++
+// C++/cpp modules have capability to create backend
+// node.js is a wrapper around Chrome V8 engine
+// that convert JS code to C++ code to create server
+
+// NPM is a command line tool
+// npm init -y = no question asked while creating package.json
+// 

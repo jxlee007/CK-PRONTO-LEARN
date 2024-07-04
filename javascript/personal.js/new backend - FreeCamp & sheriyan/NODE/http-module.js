7e9cda8f4a2a37ff1b3c1ddc7d1ca9f632b57db3
jagmohan <jimmyfalco007@gmail.com>
2024-07-04 08:28:38 +0530

    create server

---
+++ javascript/personal.js/new backend - FreeCamp & sheriyan/NODE/http-module.js
@@ -0,0 +1,12 @@
+const http = require("http");
+const cp = require("child_process");
+
+const server = http.createServer(function(req,res){
+    res.end("hello");
+});
+
+cp.execSync("start brave http://localhost:3000/");
+
+server.listen(3000);
+
+

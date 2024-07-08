e95f643a8f17f67d1b5fad9ed34744ab05315357
jagmohan <jimmyfalco007@gmail.com>
2024-07-08 07:55:12 +0530

    errow flow

--- javascript/personal.js/new backend - FreeCamp & sheriyan/NODE/server-express.js
+++ javascript/personal.js/new backend - FreeCamp & sheriyan/NODE/server-express.js
@@ -6,6 +6,26 @@ app.get('/', (req, res) => {
     res.end('Hello World!');
 });
 
-console.log("Server running on port 8000");
+
+// route params = /:param
+// route params comes in req.params
+app.get('/courses/:id', (req, res) => {
+    res.end(`You are viewing course ${req.params.id}`);
+});
+
+// requesthandler = (req, res) => {} is middleware
+// error flow
+app.get("/profile",(req,res,next) =>{
+    res.send("error");
+    return next(new Error("not working"));
+});
+
+// default error handler
+app.use((err,req,res,next)=>{
+    console.error(err.stack)
+    res.status(500).send("messed up")
+})
+
+// console.log("Server running on port 8000");
 
 // app.listen(8000);

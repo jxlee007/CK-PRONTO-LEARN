51d4c04dbb44613de1a9df344c8710caf45b91f8
jagmohan <jimmyfalco007@gmail.com>
2024-08-01 08:34:14 +0530

    Chaining Middleware

--- javascript/personal.js/new backend - FreeCamp & sheriyan/Time server.js
+++ javascript/personal.js/new backend - FreeCamp & sheriyan/Time server.js
@@ -1,9 +1,15 @@
-/*Chain Middleware to Create a Time Server 
+/*Chaining Middleware to a route
 	- Splitting server operations into smaller units using middleware has several benefits:
 	- Better App Structure: Organizes the code in a modular way, making it easier to manage and understand.
 	- Code Reusability: Allows reuse of middleware functions across different parts of the application.
 	- Data Validation: Middleware can be used to validate data at various stages of request processing.
 Error Handling: Middleware can intercept and handle errors, improving robustness and maintainability.
+
+Create a Time Server 
+In the route app.get('/now', ...) chain a middleware function and the final handler. 
+In the middleware function you should add the current time to the request object in the req.time key. 
+You can use new Date().toString(). 
+In the handler, respond with a JSON object, taking the structure {time: req.time}.
 */
 
 let express = require('express');
